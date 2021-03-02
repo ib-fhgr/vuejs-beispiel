fa4bc2e5557c123fe7a7b8e25f4c25bd37857963
ib-fhgr <ingmar.baetge@fhgr.ch>
2021-03-02 19:46:56 +0100

    first commit, added counter component

---
+++ src/components/Counter.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <span class="number">{{ number }}</span
+    ><br />
+    <button @click="up">Up</button><br />
+    <button @click="reset">Reset</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "counter",
+  data: function () {
+    return {
+      number: this.start,
+    };
+  },
+  props: {
+    start: {
+      type: Number,
+      default: 0,
+    },
+  },
+  methods: {
+    up: function () {
+      this.number++;
+    },
+    reset: function () {
+      this.number = 0;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.number {
+  font-weight: bold;
+  font-size: 2em;
+  color: red;
+}
+</style>
